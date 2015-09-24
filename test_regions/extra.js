a98677fce15dc806312aa570cf73f61a31e78d9c
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2015-09-24 11:41:01 -0400

    Rearranged folders to mimic monitor.sns.gov

--- test_regions/extra.js
+++ test_regions/extra.js
@@ -4,13 +4,13 @@ $(function(){
     $(".extra").empty()
 
     // Show anchor name
-    $(".extra").append("anchor name: ");
+    $(".extra").append("<h3>anchor name:</h3>");
     newline();
     $(".extra").append(plots[0].anchor);
     newparagraph();
 
     // Show all region names
-    $(".extra").append("region names: ");
+    $(".extra").append("<h3>region names: ");
     newline();
     for (var i=0; i<plots[0].data_region.info_table.length; i++){
       $(".extra").append(plots[0].data_region.info_table[i].region_name + ", ");
@@ -18,7 +18,7 @@ $(function(){
     newparagraph();
 
     // Show active region
-    $(".extra").append("active region: ");
+    $(".extra").append("<h3>active region:</h3>");
     newline();
     for (var i=0; i<plots[0].data_region.info_table.length; i++){
       $(".extra").append(plots[0].data_region.info_table[i].region_name + ": ");
@@ -28,7 +28,7 @@ $(function(){
     newparagraph();
 
     // Show region boundaries
-    $(".extra").append("region boundaries: ");
+    $(".extra").append("<h3>region boundaries:</h3>");
     newline();
     for (var i=0; i<plots[0].data_region.info_table.length; i++){
       $(".extra").append(plots[0].data_region.info_table[i].region_name + ": ");
@@ -39,7 +39,7 @@ $(function(){
     newparagraph();
 
     // Show region indices
-    $(".extra").append("region indices: ");
+    $(".extra").append("<h3>region indices:</h3>");
     newline();
     for (var i=0; i<plots[0].data_region.info_table.length; i++){
       $(".extra").append(plots[0].data_region.info_table[i].region_name + ": ");
@@ -50,16 +50,16 @@ $(function(){
     newparagraph();
 
     // Show data confined in each region
-    $(".extra").append("y values in each region: ");
+    $(".extra").append("<h3>y values in each region:</h3>");
     newline();
     for (var i=0; i<plots[0].data_region.info_table.length; i++){
       var region_name = plots[0].data_region.info_table[i].region_name;
       var left_idx = plots[0].data_region.info_table[i].left_idx;
       var right_idx = plots[0].data_region.info_table[i].right_idx;
-      $(".extra").append(region_name + ": ");
+      $(".extra").append("<strong>"+region_name + ":</strong>");
       newline();
       for (var j=left_idx; j<=right_idx; j++){
-        $(".extra").append(plots[0].raw_data[j][1] + ", ");
+        $(".extra").append(plots[0].raw_data[j][1] + ",");
         newline();
       }
     }
